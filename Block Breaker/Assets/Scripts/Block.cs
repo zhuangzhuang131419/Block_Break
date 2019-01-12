38ff3a40f88bd53a585554227cbfa20c6bb5cb85
Zhuang <zy22092@nottingham.edu.cn>
2019-01-12 00:18:39 -0500

    071 Reset Game Session

---
+++ Block Breaker/Assets/Scripts/Block.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block : MonoBehaviour {
+
+    [SerializeField] AudioClip breakSound;
+
+    // cached reference
+    Level level;
+
+    private void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        DestroyBlock();
+    }
+
+    private void DestroyBlock()
+    {
+        FindObjectOfType<GameSession>().AddToScore();
+        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+        Destroy(gameObject);
+        level.BlockDestroyed();   
+    }
+}
---
+++ Block Breaker/Assets/Scripts/GameSession.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameSession : MonoBehaviour {
+
+    // config params
+    [Range(0.1f, 10f)][SerializeField] float gameSpeed = 1f;
+    [SerializeField] int poinsPerBlockDestroyed = 83;
+    [SerializeField] TextMeshProUGUI scoreText;
+
+    // state variables
+    [SerializeField] int currentScore = 0;
+
+    private void Awake()
+    {
+        int gameStatusCount = FindObjectsOfType<GameSession>().Length;
+        if (gameStatusCount > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+
+    }
+
+    private void Start()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Time.timeScale = gameSpeed;
+	}
+
+    public void AddToScore()
+    {
+        currentScore += poinsPerBlockDestroyed;
+        scoreText.text = currentScore.ToString();
+    }
+
+    public void ResetGame()
+    {
+        Destroy(gameObject);
+    }
+}
